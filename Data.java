bbdb62d89d290d24005737ab52eb94f829edb162
sebseb223 <sebastienborlado@g.cjc.edu.ph>
2026-02-28 23:12:54 +0800

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ann Sherille
+ */
+public class Data {
+    
+    
+    public static String[] products = {"Creatine", "Pre-Workout", "Water", "Milk"};
+    public static double[] prices = {1200.0, 1500.0, 20.0, 85.0};
+    public static int[] stock = {20, 20, 20, 20};
+
+    public static String[] logEntries = new String[100];
+    public static int logCount = 0;
+}
